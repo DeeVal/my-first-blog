e9c6a58022bed98f43930ae3fe22c9bfcc766e32
your name <you@example.com>
2018-08-18 15:22:43 +1000

    My Django Girls app, first commit

---
+++ python_intro.py
@@ -0,0 +1,10 @@
+def hi(name):
+    print('Hi ' + name + '!')
+
+human = ['coco', 'ryle', 'betty', 'ruby', 'nya', 'levi']
+for name in human:
+    hi(name)
+    print('Next human')
+
+for i in range(1,6):
+    print(i)
